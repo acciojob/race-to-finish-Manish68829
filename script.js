da2d7723cd19108dd7f2ec9b563086208a392169
Manish <121147194+Manish68829@users.noreply.github.com>
2023-04-14 23:58:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,39 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
-const promises = [
-  new Promise((resolve) => setTimeout(() => resolve(1), Math.random() * 5000 + 1000)),
-  new Promise((resolve) => setTimeout(() => resolve(2), Math.random() * 5000 + 1000)),
-  new Promise((resolve) => setTimeout(() => resolve(3), Math.random() * 5000 + 1000)),
-  new Promise((resolve) => setTimeout(() => resolve(4), Math.random() * 5000 + 1000)),
-  new Promise((resolve) => setTimeout(() => resolve(5), Math.random() * 5000 + 1000))
-];
+var promise1=new Promise((resolve)=>{
+	const randomTime = Math.floor(Math.random() * 4000) + 1000;
+	setTimeout(()=>{
+		resolve("hello1");
+	},randomTime)
+});
+var promise2=new Promise((resolve)=>{
+	const randomTime = Math.floor(Math.random() * 4000) + 1000;
+	setTimeout(()=>{
+		resolve("hello2");
+	},randomTime)
+});
+var promise3=new Promise((resolve)=>{
+	const randomTime = Math.floor(Math.random() * 4000) + 1000;
+	setTimeout(()=>{
+		resolve("hello3");
+	},randomTime)
+});
+var promise4=new Promise((resolve)=>{
+	const randomTime = Math.floor(Math.random() * 4000) + 1000;
+	setTimeout(()=>{
+		resolve("hello4");
+	},randomTime)
+});
+var promise5=new Promise((resolve)=>{
+	const randomTime = Math.floor(Math.random() * 4000) + 1000;
+	setTimeout(()=>{
+		resolve("hello5");
+	},randomTime)
+});
+var d=document.getElementById('output');
 
-Promise.any(promises)
-  .then(result => {
-    document.getElementById("output").innerText = result;
-  })
-  .catch(error => {
-    console.log(error);
-  });
+promises=[promise1,promise2,promise3,promise4,promise5];
+Promise.any(promises).then((data)=>{
+	d.innerText=data;
+})
